c84736a80d873fee2262fb680739633ad522dd96
Kato Hawke <tkachukivan.katohawke@gmail.com>
2019-07-09 05:32:59 +0300

    Usage method <format>

--- str_format.py
+++ str_format.py
@@ -1,1 +1,7 @@
-// str_format.py file
+#!/usr/bin/env python3
+
+age = 17
+name = 'Kato'
+
+print('{0}\'s age is {1}.'.format(name, age))
+print('Why is {0} having fun with this Python?'.format(name))
