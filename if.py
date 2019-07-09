d8aa456baab66e14ccd2433086047ab1dcd1d4f1
Kato Hawke <tkachukivan.katohawke@gmail.com>
2019-07-09 17:52:36 +0300

    Usage if/elif/else construction

---
+++ if.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+number = 23
+guess = int(input('Enter integer number: '))
+
+if guess == number:
+    print('Congratulations, you guessed it,')
+    print('(although not won any prize!)')
+elif guess < number:
+    print('No, the hidden number is a little more than that.')
+else:
+    print('No, the hidden number is a little less than that.')
